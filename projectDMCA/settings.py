a1f69eea3abacb8be4af2c1cba0daafd75c3d7ef
Joshua Kim Rivera <joshuakimrivera@gmail.com>
2018-08-09 09:59:48 +0000

    LOGIN DASHBOARD PAGES

--- projectDMCA/settings.py
+++ projectDMCA/settings.py
@@ -142,6 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),os.path.join(BASE_DIR, 'static/img') ]
+
 
 LOGIN_URL = 'account:login'
 LOGIN_REDIRECT_URL = 'account:dashboard'
