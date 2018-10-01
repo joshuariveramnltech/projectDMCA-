ed20128bbff6f4ba906bbc6d9cde88e1c8930743
Kim Sunga <sungakim816@gmail.com>
2018-10-01 10:19:44 +0800

    Added add_statement.html, edit_statement.html, view_statement.html to accounting_transaction\templates

--- accounting_transaction/models.py
+++ accounting_transaction/models.py
@@ -9,16 +9,16 @@ class Statement(models.Model):
     current_school_year = GeneralSchoolYear.objects.get(id=1)
     student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE)
     school_year = models.CharField(
-        max_length=25, choices=SY, default=str(current_school_year))
+    max_length=25, choices=SY, default=str(current_school_year))
     break_down = models.TextField(null=True, blank=True)
     assessment = models.DecimalField(
-        max_digits=12, decimal_places=2, verbose_name="Assessment (Php)")
+    max_digits=12, decimal_places=2, verbose_name="Assessment (Php)")
     payment = models.DecimalField(
-        max_digits=12, decimal_places=2, verbose_name="Payment (Php)")
+    max_digits=12, decimal_places=2, verbose_name="Payment (Php)")
     balance = models.DecimalField(
-        max_digits=12, decimal_places=2, verbose_name="Balance (Php)")
+    max_digits=12, decimal_places=2, verbose_name="Balance (Php)")
     date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 
     def __str__(self):
-        return self.student.user.get_full_name + " " + self.school_year
+    return self.student.user.get_full_name + " " + self.school_year
