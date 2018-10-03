3bf3a9ca08069dbaa957579316aea34e8c4ca702
Kim Sunga <sungakim816@gmail.com>
2018-10-04 01:45:09 +0800

    finalizationPhase5

--- announcement/templatetags/announcement_tags.py
+++ announcement/templatetags/announcement_tags.py
@@ -2,3 +2,8 @@ from django import template
 from announcement.models import Announcement
 
 register = template.Library()
+
+@register.inclusion_tag('latest_school_announcement.html')
+def get_latest_school_announcements(count=5):
+    latest_school_announcements = Announcement.objects.filter(send_to_all=True).order_by('-publish_date')[:count]
+    return {'latest_school_announcements': latest_school_announcements}
