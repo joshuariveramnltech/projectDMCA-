d513b19221a2fc7adff8b21fb5b850117009a9ee
Kim Sunga <sungakim816@gmail.com>
2018-10-04 05:40:03 +0000

    Added TimeField on the admission.models.py and modify admission.views.py

--- admission/models.py
+++ admission/models.py
@@ -3,6 +3,8 @@ from django.core.validators import RegexValidator
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.text import slugify
+from datetime import datetime, date
+from django.utils import timezone
 # Create your models here.
 
 phone_regex = RegexValidator(
@@ -15,29 +17,28 @@ GENDER_CHOICES = (('male', 'Male'), ('female', 'Female'))
 
 class AppointmentRequest(models.Model):
     first_name = models.CharField(max_length=50)
-    middle_name = models.CharField(max_length=50, blank=True, default="")
+    middle_name = models.CharField(max_length=50, blank=True, null=True)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=255, unique=True)
-    age = models.CharField(max_length=10, default="", blank=True)
-    birthday = models.DateField(
-        max_length=255, blank=True, null=True,
-        help_text="Please use the format: mm/dd/yyyy")
+    age = models.CharField(max_length=10)
+    birthday = models.DateField(blank=True, null=True)
     gender = models.CharField(
-        max_length=25, blank=True, null=True,
-        choices=GENDER_CHOICES, default='male'
+        max_length=25, choices=GENDER_CHOICES,
+        default='male'
     )
-    address = models.CharField(max_length=255)
-    guardian = models.CharField(max_length=50, default="")
-    relationship = models.CharField(max_length=50)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    guardian = models.CharField(max_length=50, blank=True, null=True)
+    relationship = models.CharField(max_length=50, blank=True, null=True)
     contact_number = models.CharField(
         validators=[phone_regex, ],
         max_length=15,
-        blank=True, default='',
+        blank=True, null=True,
         help_text="Please use the format: 0999-999-9999"
     )
-    is_active = models.BooleanField(default=True, verbose_name='Active Request?')
-    schedule = models.DateField()
-    time = models.TimeField()
+    is_active = models.BooleanField(
+        default=True, verbose_name='Active Request?')
+    schedule = models.DateField(blank=True, null=True)
+    time = models.TimeField(blank=True, null=True)
     slug = models.SlugField(max_length=50, blank=True,
                             null=True, editable=False)
     date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
